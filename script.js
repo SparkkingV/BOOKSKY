a298759727f053eff19ed230d6467baa4ed8336b
vikash <vikashtech1106@gmail.com>
2025-05-12 17:43:43 +0530

    js

---
+++ script.js
@@ -0,0 +1,8 @@
+var popover =document.querySelector(".popupoverlay")
+var popbox=document.querySelector(".bookadd")
+var addpopbtn=document.getElementById("addpop")
+
+addpopbtn.addEventListener("clock",function){
+    popover.display="block"
+    popbox.display="block"
+}
