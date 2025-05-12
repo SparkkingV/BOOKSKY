f9d7f9660ce6bb4d0bc7974ccb188a04825b78d5
vikash <vikashtech1106@gmail.com>
2025-05-12 17:44:35 +0530

    js edit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var popover =document.querySelector(".popupoverlay")
 var popbox=document.querySelector(".bookadd")
 var addpopbtn=document.getElementById("addpop")
 
-addpopbtn.addEventListener("clock",function){
+addpopbtn.addEventListener("clock",function(){
     popover.display="block"
     popbox.display="block"
-}
+})
